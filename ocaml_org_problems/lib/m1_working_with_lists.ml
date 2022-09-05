c0102053731fcbf1aa90f005e2af39600fe7a772
Kirill Pavlov <k@p99.io>
2022-09-05 23:27:43 +0100

    ADD: implement n-th element accessor

--- ocaml_org_problems/lib/m1_working_with_lists.ml
+++ ocaml_org_problems/lib/m1_working_with_lists.ml
@@ -6,10 +6,17 @@ let%test _ = last [] = None
 
 (** Last two elements of a list *)
 let rec last_two = function
-  | [] -> None
-  | [ _ ] -> None
+  | [] | [ _ ] -> None
   | [ e1; e2 ] -> Some (e1, e2)
   | _ :: t -> last_two t
 
 let%test _ = last_two [ "a"; "b"; "c"; "d" ] = Some ("c", "d")
 let%test _ = last_two [ "a" ] = None
+
+(** N'th element of a list *)
+let rec list_nth n = function
+  | [] -> None
+  | h :: t -> if n = 0 then Some h else list_nth (n - 1) t
+
+let%test _ = list_nth 2 [ "a"; "b"; "c"; "d"; "e" ] = Some "c"
+let%test _ = list_nth 2 [ "a" ] = None
