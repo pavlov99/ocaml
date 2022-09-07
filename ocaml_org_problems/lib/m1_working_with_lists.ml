4f4a63fe2763900fcfe52b9db570cb2461fe8f50
Kirill Pavlov <k@p99.io>
2022-09-07 20:48:43 +0100

    REF: use pattern matching to simplify pack consecutive duplicates

--- ocaml_org_problems/lib/m1_working_with_lists.ml
+++ ocaml_org_problems/lib/m1_working_with_lists.ml
@@ -71,11 +71,12 @@ let%test _ =
 
 (** Pack consecutive duplicates *)
 let pack list =
-  let rec aux acc1 acc2 = function
-    | [] -> acc1 :: acc2
-    | h :: t ->
-        if acc1 = [] || h == List.hd acc1 then aux (h :: acc1) acc2 t
-        else aux [ h ] (acc1 :: acc2) t
+  let rec aux current acc = function
+    | [] -> []
+    | [ x ] -> (x :: current) :: acc
+    | a :: (b :: _ as t) ->
+        if a = b then aux (a :: current) acc t
+        else aux [] ((a :: current) :: acc) t
   in
   List.rev (aux [] [] list)
 
