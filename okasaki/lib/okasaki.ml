9e8b0d282ca7362038fb6182d1423db91d4452cb
Kirill Pavlov <k@p99.io>
2022-09-09 22:35:55 +0100

    REF: encapsulate tree methods in UnbalancedSet module

--- okasaki/lib/okasaki.ml
+++ okasaki/lib/okasaki.ml
@@ -1,24 +1,29 @@
-type 'a tree = Empty | Node of 'a tree * 'a * 'a tree
+module UnbalancedSet = struct
+  type 'a tree = Empty | Node of 'a tree * 'a * 'a tree
 
-let rec mem t el =
-  match t with
-  | Empty -> false
-  | Node (left, value, right) ->
-      if el = value then true
-      else if el < value then mem left el
-      else mem right el
+  let empty = Empty
 
-let%test _ = mem Empty 0 = false
-let%test _ = mem (Node (Empty, 0, Empty)) 0 = true
-let%test _ = mem (Node (Node (Empty, -1, Empty), 0, Empty)) (-1) = true
+  let rec mem el = function
+    | Empty -> false
+    | Node (left, value, right) ->
+        if el = value then true
+        else if el < value then mem el left
+        else mem el right
 
-let rec insert t el =
-  match t with
-  | Empty -> Node (Empty, el, Empty)
-  | Node (l, v, r) ->
-      if el <= v then Node (insert l el, v, r) else Node (l, v, insert r el)
+  let rec insert el = function
+    | Empty -> Node (Empty, el, Empty)
+    | Node (l, v, r) ->
+        if el <= v then Node (insert el l, v, r) else Node (l, v, insert el r)
+end
 
-let%test _ = insert Empty 0 = Node (Empty, 0, Empty)
+let%test _ = UnbalancedSet.mem 0 Empty = false
+let%test _ = UnbalancedSet.mem 0 (Node (Empty, 0, Empty)) = true
 
 let%test _ =
-  insert (Node (Empty, 0, Empty)) 1 = Node (Empty, 0, Node (Empty, 1, Empty))
+  UnbalancedSet.mem (-1) (Node (Node (Empty, -1, Empty), 0, Empty)) = true
+
+let%test _ = UnbalancedSet.insert 0 Empty = Node (Empty, 0, Empty)
+
+let%test _ =
+  UnbalancedSet.insert 1 (Node (Empty, 0, Empty))
+  = Node (Empty, 0, Node (Empty, 1, Empty))
